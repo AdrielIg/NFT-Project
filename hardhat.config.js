337f4502a2e86ed433a66dd31cb6485cf902f301
AdrielIg <adrielgomezignacio@gmail.comg>
2022-01-07 20:39:51 -0300

    Add private key and infura id

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,13 +1,15 @@
 require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
+
+const proejctId = process.env.INFURA_PROJECT_ID;
+const privateKey = process.env.DEPLOYER_SIGNER_PRIVATE_KEY;
 
 module.exports = {
   solidity: "0.8.4",
   networks: {
     rinkeby: {
-      url: "https://rinkeby.infura.io/v3/f05bbeb7f5b747cf9ff3a9434b8f9a87",
-      accounts: [
-        "0x77a9d0065229ab2968b2b0f6c8719556a1235d4237a8f6e6e8a26615180c2669",
-      ],
+      url: `https://rinkeby.infura.io/v3/${proejctId}`,
+      accounts: [privateKey],
     },
   },
 };
